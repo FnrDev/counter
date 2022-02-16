159e94b3b695eea3358989bdaef5a0fb2f700243
Fnr <ah2014bh@outlook.com>
2022-02-16 13:30:58 +0300

    use h1 tag instead of input

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let currentInputValue = document.getElementById('todo').value;
+let currentInputValue = document.getElementById('todo').innerHTML;
 const addButton = document.getElementById('add-btn');
 const removeButton = document.getElementById('remove-btn');
 const resetButton = document.getElementById('reset-btn');
@@ -6,18 +6,18 @@ const resetButton = document.getElementById('reset-btn');
 // Add event listener for "Add" button.
 addButton.addEventListener('click', () => {
     currentInputValue++
-    document.getElementById('todo').value = currentInputValue;
+    document.getElementById('todo').innerHTML = currentInputValue;
 });
 
 // Add event listener for "Remove" button.
 removeButton.addEventListener('click', () => {
     if (currentInputValue < 0) return;
     currentInputValue--
-    document.getElementById('todo').value = currentInputValue;
+    document.getElementById('todo').innerHTML = currentInputValue;
 });
 
 // Add event listener for "Reset" button.
 resetButton.addEventListener('click', () => {
     currentInputValue = 0;
-    document.getElementById('todo').value = 0;
+    document.getElementById('todo').innerHTML = 0;
 });
